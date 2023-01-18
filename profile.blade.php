dcb997b4e7efe9d7eed335e3ddd34b67a6b57f83
Genozee <122953683+Genozee@users.noreply.github.com>
2023-01-18 15:11:22 +0700

    Add files via upload

---
+++ profile.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profile</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <CENTER>
+    <h2 style="text-align: center">Biodata</h2>
+    <p>Nama Lengkap: Mokhamad Zaidan Fatih</p>
+    <p>Jenis Kelamin: Pria</p>
+    <p>Asal Sekolah: SMK BPI Bandung</p>
+    <p>Kelas: XI RPL</p>
+    <p>Tempat Lahir: Bandung</p>
+    <p>Tanggal Lahir: 17 April 2006</p>
+    <p>Agama: Islam</p>
+    <p>Status: Belum Menikah</p>
+    </CENTER>
+ 
+    <img src="download.jpg">  
+</body>
+</html>
---
+++ web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    return view ('Welcome');
+});
+
+Route::get('/home', function () {
+    return ('Halaman Home');
+});
+
+Route::get('/about', function () {
+    return ('Halaman About');
+});
+
+Route::get('/blog', function () {
+    return ('Halaman Blog');
+});
+
+Route::get('/fakta', function () {
+    return ('Lautan Fakta');
+});
+
+Route::get('/belajar', function () {
+    echo '<h1>Hello World!</h1>';
+    echo '<p>Sedang belajar Laravel</p>';
+});
+
+Route::get('/siswa/smkbpi/rpl', function () {
+    echo '<h2 style="text-align: center"><u>Welcome To 11 RPL</u></h2>';
+});
+
+Route::get('/siswa/{zaidan}', function ($zaidan) {
+    return "Tampilkan data siswa bernama $zaidan";
+});
+
+Route::get('/stok_barang/{jenis}/{merek}', function ($jenis,$merek) {
+    return "Cek sisa stok untuk $jenis $merek";
+});
+
+Route::get('/profile', function () {
+    return view ('profile');
+});
